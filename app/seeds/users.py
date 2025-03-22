5ac0f36770cb618e7a3973c9ae299515cd2a326e
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-03-21 23:39:57 -0400

    added firstname and lastname to model, seeder, and migrations

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,11 +5,11 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-       firstname='Demo', lastname='User', username='Demo', email='demo@aa.io', password='password')
+        firstname='Demo', lastname='User', email='demo@aa.io', password='password')
     marnie = User(
-        firstname='MAR', lastname='MARNI', username='marnie', email='marnie@aa.io', password='password')
+        firstname='MAR', lastname='MARNI', email='marnie@aa.io', password='password')
     bobbie = User(
-        firstname='BOB', lastname='BOBBIE', username='bobbie', email='bobbie@aa.io', password='password')
+        firstname='BOB', lastname='BOBBIE', email='bobbie@aa.io', password='password')
 
     db.session.add(demo)
     db.session.add(marnie)
