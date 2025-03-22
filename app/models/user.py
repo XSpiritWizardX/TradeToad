45ea3c67d49cf3c5fbc5a9d0dc8cc8844e32b668
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-03-22 00:42:26 -0400

    version migration files???

--- app/models/user.py
+++ app/models/user.py
@@ -10,9 +10,9 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(40), nullable=False)
-    lastname = db.Column(db.String(40), nullable=False)
-    # username = db.Column(db.String(40), nullable=False, unique=True)
+    # firstname = db.Column(db.String(40), nullable=False)
+    # lastname = db.Column(db.String(40), nullable=False)
+    username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
@@ -30,9 +30,9 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
-            'firstname': self.firstname,
-            'lastname': self.lastname,
-            # 'username': self.username,
+            # 'firstname': self.firstname,
+            # 'lastname': self.lastname,
+            'username': self.username,
             'email': self.email
 
         }
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,15 +5,15 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        firstname='Demo', lastname='User', email='demo@aa.io', password='password')
-    marnie = User(
-        firstname='MAR', lastname='MARNI', email='marnie@aa.io', password='password')
-    bobbie = User(
-        firstname='BOB', lastname='BOBBIE', email='bobbie@aa.io', password='password')
+        username="Demo", email='demo@aa.io', password='password')
+    # marnie = User(
+    #     firstname='MAR', lastname='MARNI', email='marnie@aa.io', password='password')
+    # bobbie = User(
+    #     firstname='BOB', lastname='BOBBIE', email='bobbie@aa.io', password='password')
 
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    # db.session.add(marnie)
+    # db.session.add(bobbie)
     db.session.commit()
 
 
