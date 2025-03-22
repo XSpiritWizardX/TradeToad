b59e718fad862bbed902b3e6a0b3b14d7751a559
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-03-21 21:46:21 -0400

    setup for render

--- react-vite/src/components/Navigation/ProfileButton.jsx
+++ react-vite/src/components/Navigation/ProfileButton.jsx
@@ -2,11 +2,20 @@ import { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { FaUserCircle } from 'react-icons/fa';
 import { thunkLogout } from "../../redux/session";
+
+
 import OpenModalMenuItem from "./OpenModalMenuItem";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
 
+import './ProfileButton.css'
+
+
+
 function ProfileButton() {
+
+
+
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
   const user = useSelector((store) => store.session.user);
@@ -39,6 +48,15 @@ function ProfileButton() {
     closeMenu();
   };
 
+
+
+
+
+
+
+
+
+  
   return (
     <>
       <button onClick={toggleMenu}>
