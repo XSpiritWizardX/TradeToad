cddea2f8f5ff2ba109912b22202c837036c3184b
Heather Davidson <davidsonheather31@gmail.com>
2025-04-02 16:57:13 -0700

    installing stocks graph

--- react-vite/src/components/Portfolio/Portfolio.jsx
+++ react-vite/src/components/Portfolio/Portfolio.jsx
@@ -51,7 +51,68 @@ function PortfolioCard() {
 
 
       </div>
-      {/* End react charts here */}
+
+      {/* start react charts here */}
+type DailyStars = {
+   date: Date,
+   stars: number,
+ }
+
+ type Series = {
+   label: string,
+   data: DailyStars[]
+ }
+
+ const data: Series[] = [
+   {
+     label: 'React Charts',
+     data: [
+       {
+         date: new Date(),
+         stars: 202123,
+       }
+       // ...
+     ]
+   },
+   {
+     label: 'React Query',
+     data: [
+       {
+         date: new Date(),
+         stars: 10234230,
+       }
+       // ...
+     ]
+   }
+ ]
+ function App() {
+   const primaryAxis = React.useMemo(
+     (): AxisOptions<DailyStars> => ({
+       getValue: datum => datum.date,
+     }),
+     []
+   )
+
+   const secondaryAxes = React.useMemo(
+     (): AxisOptions<DailyStars>[] => [
+       {
+         getValue: datum => datum.stars,
+       },
+     ],
+     []
+   )
+
+   return (
+     <Chart
+       options={{
+         data,
+         primaryAxis,
+         secondaryAxes,
+       }}
+     />
+   )
+ }
+{/** end of chart */}
 
     </div>
   );
