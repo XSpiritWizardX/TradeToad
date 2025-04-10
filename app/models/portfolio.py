3d077d6b032bb69c65da88c33298af10d800ccae
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-04-09 22:27:22 -0400

    user relationships done

--- app/models/portfolio.py
+++ app/models/portfolio.py
@@ -7,9 +7,14 @@ class Portfolio(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
     total_cash = db.Column(db.Numeric(precision=15, scale=2, asdecimal=True), nullable=False)
-    available_cash = db.Column(db.Numeric(precision=15, scale=2), nullable=False)
+    available_cash = db.Column(db.Numeric(precision=15, scale=2, asdecimal=True), nullable=False)
+
+
+     # Relationships
+    # portfolio_stocks = db.relationship("PortfolioStock", back_populates="portfolio", cascade="all, delete-orphan")
+    user = db.relationship("User", back_populates="portfolio")
 
 
     def to_dict(self):
--- app/models/stock_transaction.py
+++ app/models/stock_transaction.py
@@ -8,16 +8,16 @@ class Stock_Transaction(db.Model):
         __table_args__ = {"schema": SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    # user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
     # stock_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("stocks.id")), nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
+    # user_id = db.Column(db.Integer, nullable=False)
     stock_id = db.Column(db.Integer, nullable=False)
     shares = db.Column(db.Numeric(precision=15, scale=2, asdecimal=True), nullable=False)
     price = db.Column(db.Numeric(precision=15, scale=2, asdecimal=True), nullable=False)
     action = db.Column(db.String(4), nullable=False)
 
     # Relationships
-    # user = db.relationship("User", back_populates="stock_transactions")
+    user = db.relationship("User", back_populates="stock_transactions")
     # stock = db.relationship("Stock", back_populates="stock_transactions")
 
     def to_dict(self):
