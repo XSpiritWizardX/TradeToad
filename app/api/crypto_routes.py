e0822c6b03272a337259465ecfda67f19f96ab1c
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-04-12 20:41:28 -0400

    added get all cryptos route

--- app/api/crypto_routes.py
+++ app/api/crypto_routes.py
@@ -1,7 +1,20 @@
-from flask import Blueprint, request
+from flask import Blueprint, jsonify, request
 from app.models import Crypto, db
 
 
 
 
 crypto_routes = Blueprint('cryptos', __name__, "")
+
+
+
+
+
+# works good
+@crypto_routes.route('/')
+def get_cryptos():
+    """
+    Get all cryptos.
+    """
+    cryptos = Crypto.query.all()
+    return jsonify({'cryptos': [crypto.to_dict() for crypto in cryptos]})
