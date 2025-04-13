e8b7a75d3c7735c0e7e3507d8b4d1b306a6f7488
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-04-12 21:27:10 -0400

    added watchlist crypto routes

--- app/api/watchlist_crypto_routes.py
+++ app/api/watchlist_crypto_routes.py
@@ -1,7 +1,63 @@
-from flask import Blueprint, request
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
 from app.models import Watchlist_Crypto, db
 
 
 
 
 watchlist_crypto_routes = Blueprint('watchlist_cryptos', __name__, "")
+
+
+
+
+
+@watchlist_crypto_routes.route('/')
+@login_required
+def get_watchlist_cryptos():
+    """
+    Get all watchlist_cryptos for the logged-in user.
+    """
+    watchlist_cryptos = Watchlist_Crypto.query.filter_by(user_id=current_user.id).all()
+    return jsonify({'watchlist_cryptos': [watchlist_crypto.to_dict() for watchlist_crypto in watchlist_cryptos]})
+
+
+
+
+@watchlist_crypto_routes.route('/', methods=['POST'])
+@login_required
+def create_watchlist_crypto():
+    """
+    Create a new watchlist_crypto for the logged-in user.
+    """
+    data = request.get_json()
+    watchlist_id = data.get("watchlist_id")
+    crypto_id = data.get("crypto_id")
+
+
+
+    new_watchlist_crypto = Watchlist_Crypto(user_id=current_user.id, watchlist_id=watchlist_id, crypto_id=crypto_id)
+    db.session.add(new_watchlist_crypto)
+    db.session.commit()
+
+    return jsonify(new_watchlist_crypto.to_dict()), 201
+
+
+
+
+
+
+
+@watchlist_crypto_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_watchlist_crypto(id):
+    """
+    Delete a user's watchlist_crypto.
+    """
+    watchlist_crypto = Watchlist_Crypto.query.filter_by(id=id, user_id=current_user.id).first()
+
+    if not watchlist_crypto:
+        return jsonify({'error': 'Watchlist_Crypto not found'}), 404
+
+    db.session.delete(watchlist_crypto)
+    db.session.commit()
+    return jsonify({'message': 'Watchlist_Crypto deleted successfully'})
